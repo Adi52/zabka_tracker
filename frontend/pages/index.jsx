4569b4bc5ea4046107548ba684a59548c3eb6ba1
Adrian Bieliński <adrian@bielinski.dev>
2021-10-17 17:48:12 +0200

    Create dashboard page

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,7 +1,40 @@
-const HomePage = () => {
-  console.log("a");
+import { Layout, Typography } from "antd";
+import Map from "../components/Map";
+import getMarkersList from "../helpers/api/getMarkersList";
 
-  return <div>Home</div>;
-};
+const { Content } = Layout;
+const { Title } = Typography;
+
+const HomePage = ({ markers }) => (
+  <Content className="site-layout" style={{ padding: "0 50px", marginTop: 64 }}>
+    <Title level={3}>Dashboard</Title>
+    <div
+      className="site-layout-content"
+      style={{
+        padding: 24,
+        minHeight: "78vh",
+        display: "flex",
+      }}
+    >
+      <Map markers={markers} />
+    </div>
+  </Content>
+);
+
+export async function getServerSideProps() {
+  const [error, response] = await getMarkersList();
+
+  if (error) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      markers: response,
+    },
+  };
+}
 
 export default HomePage;
